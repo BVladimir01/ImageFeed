f78d56b2fa28e32dada238c3b13c1e57997042d3
Vladimir <bizin.vladimir01@gmail.com>
2025-03-03 16:52:09 +0300

    add constants for web

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Vladimir on 28.02.2025.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "qEjC0CE_szJexm7g2JaWECzrSUrpoXe32le86UJR1vA"
+    static let secretKey = "yWxkzZQmpelJn9tzHpnDcRXwk253mICdvFN91cRfU-E"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
+}
