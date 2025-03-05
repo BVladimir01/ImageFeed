3b9abde60b543bfb6525c538375b6097711d518c
Vladimir <bizin.vladimir01@gmail.com>
2025-03-05 17:14:42 +0300

    add URLSession+data extension for handling URLSessionDataTasks

---
+++ ImageFeed/Extensions/URLSession+data.swift
@@ -0,0 +1,38 @@
+//
+//  URLSession+data.swift
+//  ImageFeed
+//
+//  Created by Vladimir on 05.03.2025.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
+    case urlSessionError
+}
+
+extension URLSession {
+    func data(for request: URLRequest, completion: @escaping (Result<Data,Error>) -> Void) -> URLSessionDataTask {
+        let mainThreadCompletionHandler: (Result<Data, Error>) -> Void = { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+        let task = dataTask(with: request) { data, response, error in
+            if let data, let response, let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if 200..<300 ~= statusCode {
+                    mainThreadCompletionHandler(.success(data))
+                } else {
+                    mainThreadCompletionHandler(.failure(NetworkError.httpStatusCode(statusCode)))
+                }
+            } else if let error {
+                mainThreadCompletionHandler(.failure(NetworkError.urlRequestError(error)))
+            } else {
+                mainThreadCompletionHandler(.failure(NetworkError.urlSessionError))
+            }
+        }
+        return task
+    }
+}
