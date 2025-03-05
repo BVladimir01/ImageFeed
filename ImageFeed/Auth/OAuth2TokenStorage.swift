e1389a7551e1a7f8965cbb538e7113ed88c6102e
Vladimir <bizin.vladimir01@gmail.com>
2025-03-05 17:45:53 +0300

    add token storage

---
+++ ImageFeed/Auth/OAuth2TokenStorage.swift
@@ -0,0 +1,22 @@
+//
+//  OAuth2TokenStorage.swift
+//  ImageFeed
+//
+//  Created by Vladimir on 05.03.2025.
+//
+
+import Foundation
+
+class OAuth2TokenStorage {
+    private let tokenKey = "tokenKey"
+    private let storage = UserDefaults.standard
+    
+    var token: String? {
+        get {
+            storage.string(forKey: tokenKey)
+        }
+        set {
+            storage.set(newValue, forKey: tokenKey)
+        }
+    }
+}
