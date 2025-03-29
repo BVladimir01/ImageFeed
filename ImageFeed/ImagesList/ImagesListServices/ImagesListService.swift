7ef1dbf4d733a0b4afe6322fef71e31a6120889e
Vladimir <bizin.vladimir01@gmail.com>
2025-03-29 15:09:33 +0300

    add methods to ImagesListService

--- ImageFeed/ImagesList/ImagesListServices/ImagesListService.swift
+++ ImageFeed/ImagesList/ImagesListServices/ImagesListService.swift
@@ -9,13 +9,64 @@ import Foundation
 
 final class ImagesListService {
     
+    static let didChangeNotification = Notification.Name("ImagesListServiceDidChange")
+    
     private(set) var photos: [Photo] = []
     
     private var task: URLSessionTask?
     private var lastLoadedPage: Int?
-    
+    private let pathString = "/photos"
+    private let itemsPerPage = 10
+    private let tokenStorage = OAuth2TokenStorage.shared
+    private let urlSession = URLSession.shared
     
     func fetchPhotosNextPage() {
         //TODO: implement fetching photos
+        let nextPageNumber = (lastLoadedPage ?? 0) + 1
+        guard let request = urlRequest(page: nextPageNumber) else { return }
+        if let task {
+            print("ImagesListService.fetchPhotosNextPage: duplicating request for photos")
+            return
+        }
+        // just in case
+        task?.cancel()
+        task = urlSession.objectTask(for: request) { [weak self] (result: Result<[PhotoResult], Error>) in
+            guard let self else { return }
+            switch result {
+            case .success(let photoResults):
+                self.photos.append(contentsOf: photoResults.map({ Photo(photoResult: $0) }))
+                NotificationCenter.default.post(name: ImagesListService.didChangeNotification, object: self)
+                self.lastLoadedPage = nextPageNumber
+                self.task = nil
+            case .failure(let error):
+                print("ImagesListService.fetchPhotosNextPage error")
+            }
+        }
+        task?.resume()
+        
+    }
+    
+    private func urlRequest(page: Int) -> URLRequest? {
+        guard var urlComponents = URLComponents(string: Constants.defaultBaseURLString) else {
+            assertionFailure("ImagesListService.urlRequest: Failed to create url components from base url string for unsplash")
+            return nil
+        }
+        guard let token = tokenStorage.token else {
+            assertionFailure("ImagesListService.urlRequest: Failed to get token from storage")
+            return nil
+        }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "page", value: page.description),
+            URLQueryItem(name: "per_page", value: itemsPerPage.description)
+        ]
+        urlComponents.path = pathString
+        guard let assembledURL = urlComponents.url else {
+            assertionFailure("ImagesListService.urlRequest: Failed to create url with filled in query items")
+            return nil
+        }
+        var urlRequest = URLRequest(url: assembledURL)
+        urlRequest.httpMethod = HTTPMethod.get.rawValue
+        urlRequest.setValue("Bearer ", forHTTPHeaderField: "Authorization")
+        return urlRequest
     }
 }
