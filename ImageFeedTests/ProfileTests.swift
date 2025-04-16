05cea212bf529f13eaffeb457214f86721c67450
Vladimir <bizin.vladimir01@gmail.com>
2025-04-16 10:20:45 +0300

    add some tests for ProfileVC and ProfilePresenter

--- ImageFeedTests/ProfileTests.swift
+++ ImageFeedTests/ProfileTests.swift
@@ -51,8 +51,7 @@ final class ProfileTests: XCTestCase {
         func setProfileImage(url: URL) {
             didCallSetProfileImage = true
         }
-        
-        
+
     }
     
     func testViewControllerCallsViewDidLoad() {
@@ -80,5 +79,37 @@ final class ProfileTests: XCTestCase {
         //then
         XCTAssertTrue(profileVCSpy.didCallShowLogoutAlert)
     }
+    
+    
+    // can not test ProfileViewController.setProfileDetails
+    // no access to text of labels
+    // will probably test in UI tests
+    
+    // can not test ProfileViewController.SetProfileImage
+    // no access to profile image
+    // will probably test in UI tests
+    
+    func testConfirmLogout() {
+        // given
+        let presenter = ProfilePresenter()
+        
+        // when
+        presenter.confirmLogout()
+        
+        // then
+        XCTAssertNil(OAuth2TokenStorage.shared.token)
+        XCTAssertNil(ProfileService.shared.profile)
+        XCTAssertNil(ProfileImageService.shared.avatarURL)
+        XCTAssertTrue(ImagesListService.shared.photos.isEmpty)
+        // critical part of logout that does not involve UI
+        // does not seem to be a valid test, services contain info
+        // only after real session
+    }
+    
+    // for proper testing of presenter and VC
+    // all services should be conforming protocols
+    // all services should be injected as dependancies
+    // not shared among all classes
+    // then stubs could be used for testing
     
 }
